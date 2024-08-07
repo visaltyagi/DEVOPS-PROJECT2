36ebe79bea91477adac880871c3af17bc496cf5c
VISAL TYAGI <107295081+visaltyagi@users.noreply.github.com>
2024-08-07 18:09:22 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install openjdk-17-jre-headless -y
+sudo apt-get install docker.io -y
+sudo systemctl enable --now docker
+sudo swapoff -a
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg - dearmor -o /etc/apt/keyringsecho 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/dsudo apt-get updateetc/apt/sources>
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
